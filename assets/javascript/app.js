6c07a814716f74c8a100487a22234e8a5b4d1e2f
Carolyn Bernier <carolynbbernier@gmail.com>
2018-09-18 15:43:36 -0400

    attempted to make variables to contain answers that are correct and incorrect. attempted to check for correct and incorrect answers.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,15 @@
 
 $(document).ready(function() {
+
+   
+//two variables, one for keeping score and the other for counting down time.
+
 var score= 0;
 var counter = 10;
 
-$("#wrapper").show();
 
+
+//Function that counts down the seconds and displays the counter in HTML.
 
 function counterFunction() {
     counter = counter-1;
@@ -12,22 +17,76 @@ function counterFunction() {
     return counter;
 };
 
+//When "start" is clicked, the counter counts down.
+
  
 $("#start").on("click", setInterval(counterFunction, 1000));
 
 
+//if the counter has hit zero, the inverval is cleared, meaning the counter vanishes. The questions disappear.
+
 if (counter = 0) {
-    clearInterval(); }
+    clearInterval(counter); 
+    $(".questions").hide();
+}
+
+// if the user hits the "done" button, the questions are supposed to hide and the counter is supposed to stop. 
+
+$("#done").click(function() {
+    clearInterval(counter);
+    $(".questions").hide();
+});
 
 
-var correctAnswer = documents.getElementByClass(".correctAnswer");
-   if (correctAnswer.checked) {
-        score = score + 1;
-    };
+//a variable is created that checks the correct answers. 
+
+var correctAnswer = 0;
+
+//the correctAnswer variable is somehow responsive to the correctAnswer HTML class. 
+
+var correctAnswer = document.getElementByClass(".correctAnswer");
+
+//incorrect answer element has a counter of zero.
+
+var incorrectAnswer = 0;
+
+//the incorrect answers variable somehow collects infortion on the incorrect answers in the HTML.
+
+var incorrectAnswer = document.getElementByClass("incorrectAnswer");
+
+var unanswered = 0;
+
+//For every correctAnswer radio button that's checked, the score is increased by one. 
+// Calculate the incorrect answers. 
+
+if (correctAnswer.checked() = true) {
+        score++;
+        return score;
+    }
+
+//the number of incorrect answers are checked and added to the variable incorrectAnswers.
+//incorrectAnswers is returned.
+
+
+else if (incorrectAnswer.checked() = true) {
+    incorrectAnswers++;
+    return incorrectAnswers;
+}
+
+//unanswered answers are calculated by subtracting the correct answers and incorrect answers from the total number
+//of questions, which is 4. 
+
+else () {
+    unanswered = (4-(correctAnswer + incorrectAnwer));
+    return unanswered;
+};
+
 
     
-    $("#done").click(function() {
-        $("#wrapper").hide();
-    });
+   
+
+    
+
+
 
 });
