184c49c8d17514e7dfb5a52d4b54abf82c8a323d
Carolyn Bernier <carolynbbernier@gmail.com>
2018-09-18 14:20:49 -0400

    tried to add score, clear interval, done button functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,33 @@
 
+$(document).ready(function() {
+var score= 0;
+var counter = 10;
 
-var score=0;
-var counter = 160;
+$("#wrapper").show();
+
 
 function counterFunction() {
     counter = counter-1;
     $("#timer").text(counter);
-return counter;}
-
-$("#start").on("click", setInterval(counterFunction(), 1000));
-
-
-
-for (i=0; i >= 0; i++ ) {
-    
-
+    return counter;
 };
+
+ 
+$("#start").on("click", setInterval(counterFunction, 1000));
+
+
+if (counter = 0) {
+    clearInterval(); }
+
+
+var correctAnswer = documents.getElementByClass(".correctAnswer");
+   if (correctAnswer.checked) {
+        score = score + 1;
+    };
+
+    
+    $("#done").click(function() {
+        $("#wrapper").hide();
+    });
+
+});
