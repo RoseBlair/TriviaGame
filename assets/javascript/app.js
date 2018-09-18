8a7247e2be040d1e93fcf46d0cd1dd676f61de68
Carolyn Bernier <carolynbbernier@gmail.com>
2018-09-18 13:38:51 -0400

    trying to make the timer work. made labels for radio button.

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+
+
+var score=0;
+var counter = 160;
+
+function counterFunction() {
+    counter = counter-1;
+    $("#timer").text(counter);
+return counter;}
+
+$("#start").on("click", setInterval(counterFunction(), 1000));
+
+
+
+for (i=0; i >= 0; i++ ) {
+    
+
+};
